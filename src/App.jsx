d0af5c9d916565cf56de9e575383bcdf74040dd9
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-11 15:51:35 +0100

    add: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import AppHeader from './components/AppHeader'
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-
+      <AppHeader />
     </>
   )
 }
---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,11 @@
+const title = "Task Manager"
+
+export default function AppHeader() {
+    return (
+        <header>
+            <div>
+                <h2>{title}</h2>
+            </div>
+        </header>
+    )
+}
